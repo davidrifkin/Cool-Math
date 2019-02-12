e9492a5dbad378ec03617404b925ac765a1116f0
davidrifkin <davidlrifkin1@gmail.com>
2019-02-12 14:09:47 -0500

    We created our math engine

---
+++ MathEngine.swift
@@ -0,0 +1,30 @@
+#!/usr/bin/swift
+import Foundation
+
+class CoolMath {
+  func addition(_ a:Int, _ b:Int) {
+    printCool(result: (a+b))
+  }
+  
+  func subtraction(_ a:Int, _ b:Int) {
+    printCool(result: (a-b))
+  }
+  
+  func multiplication(_ a:Int, _ b:Int) {
+    printCool(result: (a*b))
+  }
+  
+  func division(_ a:Int, _ b:Int) {
+    printCool(result: (a/b))
+  }
+  
+  private func printCool(result:Int) {
+    print("Cool, the answer is", result)
+  }
+}
+
+var mathBoy = CoolMath()
+mathBoy.addition(2, 3)
+mathBoy.subtraction(1, 2)
+mathBoy.multiplication(1, 2)
+mathBoy.division(2,1)
